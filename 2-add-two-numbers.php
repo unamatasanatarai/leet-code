a4ea3dc782f2ba25974f79ea2db650571320fc48
UnamataSanatarai <UnamataSanatarai@gmail.com>
2025-12-27 22:42:46 +0100

    https://leetcode.com/problems/add-two-numbers/description/

--- 2-add-two-numbers.php
+++ 2-add-two-numbers.php
@@ -1,63 +1,83 @@
 <?php
 
-/**
- * Definition for a singly-linked list.
- * class ListNode {
- *     public $val = 0;
- *     public $next = null;
- *     function __construct($val = 0, $next = null) {
- *         $this->val = $val;
- *         $this->next = $next;
- *     }
- * }
- */
-
 class ListNode
 {
-    public $data;
+    public $val;
     public $next;
 
-    public function __construct($data)
+    public function __construct($val)
     {
-        $this->data = $data;
+        $this->val = $val;
         $this->next = null;
     }
 }
+
 class Solution
 {
-    /**
-     * @param ListNode $l1
-     * @param ListNode $l2
-     * @return ListNode
-     */
     public function addTwoNumbers($l1, $l2)
     {
-        $now = new ListNode(0);
-        $wip = $now;
+        $root = new ListNode(0);
+        $tmp = $root;
         $carry = 0;
 
         while ($l1 || $l2) {
-            $x = ($l1 != null) ? $l1->val : 0;
-            $y = ($l2 != null) ? $l2->val : 0;
-            $sum = $carry + $x + $y;
-            $carry = intdiv($sum, 10);
-            $wip->next = new ListNode($sum % 10);
-            $wip = $wip->next;
-            if ($l1 != null) {
-                $l1 = $l1->next;
-            }
-            if ($l2 != null) {
-                $l2 = $l2->next;
-            }
+            $r = ($l1->val ?? 0) + ($l2->val ?? 0) + $carry;
 
+            $carry = (int)($r / 10);
+            $tmp->next = new ListNode($r % 10);
+            $tmp = $tmp->next;
+
+            $l1 = $l1->next ?? null;
+            $l2 = $l2->next ?? null;
         }
+
         if ($carry) {
-            $wip->next = new ListNode($carry);
+            $tmp->next = new ListNode($carry);
         }
-        return $now->next;
+
+        return $root->next;
     }
 }
 
+$test_cases = [
+    [
+        [9,9,9,9,9,9,9],
+        [9,9,9,9],
+        [8,9,9,9,0,0,0,1],
+    ],
+    [
+        [2,4,3],
+        [5,6,4],
+        [7,0,8],
+    ],
+];
+
+function makeNodes($arr)
+{
+    $current = new ListNode(array_shift($arr));
+    $tmp = $current;
+    while ($arr) {
+        $tmp->next = new ListNode(array_shift($arr));
+        $tmp = $tmp->next;
+    }
+    return $current;
+}
+
 $sol = new Solution();
-$sol->addTwoNumbers([2,4,3], [5,6,4]);
-//#[7,0,8]
+foreach ($test_cases as $test_case) {
+    $lft = makeNodes($test_case[0]);
+    $rgt = makeNodes($test_case[1]);
+    $exp = makeNodes($test_case[2]);
+    $res = $sol->addTwoNumbers($lft, $rgt);
+    while ($exp) {
+        $a = $res->val;
+        $b = $exp->val;
+        if ($a != $b) {
+            echo "FAILED";
+            print_r($test_case);
+            break;
+        }
+        $res = $res->next;
+        $exp = $exp->next;
+    }
+}
